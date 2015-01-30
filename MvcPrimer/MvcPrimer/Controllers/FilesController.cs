83e2d9249436d01e1e0a0a3218f5e366076edd7e
alan-cy-lai <alan@ergostratus.com>
2015-01-29 19:39:10 -0800

    W04A MVC with Files

---
+++ MvcPrimer/MvcPrimer/Controllers/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Web.Hosting;
+
+namespace MvcPrimer.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+            string[] filenames = Directory.GetFiles(HostingEnvironment.MapPath("~/textfiles"));
+            return View(filenames);
+        }
+
+        public ActionResult ShowContent(string path)
+        {
+            if (path == null) { return View(); }
+            ViewBag.Content = System.IO.File.ReadAllText(path);
+            return View();
+        }
+    }
+}
