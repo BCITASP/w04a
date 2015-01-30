67a841b507da152b61819596cb0e7bbc1a7c782b
alan-cy-lai <alan@ergostratus.com>
2015-01-29 21:00:39 -0800

    /Files Routing Fixed it so that it now routes properly to Files page by calling the existing Index function

--- MvcPrimer/MvcPrimer/App_Start/RouteConfig.cs
+++ MvcPrimer/MvcPrimer/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace MvcPrimer
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.RouteExistingFiles = true;
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
